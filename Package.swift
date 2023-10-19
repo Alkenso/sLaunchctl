54089e01c796366d75dae67ea4659632ccb40df6
Alkenso (Vladimir Vashurkin) <alkensox@gmail.com>
2023-10-19 09:45:34 +0300

    Update SwiftSpellbook to 0.3.1

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/Alkenso/SwiftSpellbook.git", exact: "0.3.0"),
+        .package(url: "https://github.com/Alkenso/SwiftSpellbook.git", from: "0.3.1"),
     ],
     targets: [
         .target(
