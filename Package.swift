3055d43bd79ba34f013f0add76e0bee5b5a1acce
Alkenso (Vladimir Vashurkin) <alkensox@gmail.com>
2023-12-12 15:53:41 +0200

    Bump min supported version to 10.15

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "sLaunchctl",
-    platforms: [.macOS(.v10_13)],
+    platforms: [.macOS(.v10_15)],
     products: [
         .library(
             name: "sLaunchctl",
